39928bd822b144cee94fae598d040780dbc9c5b2
jehwan <lluns67@gmail.com>
2026-02-09 15:22:21 +0900

    first commit

---
+++ soragodong/src/main/java/com/scit/soragodong/SoragodongApplication.java
@@ -0,0 +1,13 @@
+package com.scit.soragodong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoragodongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoragodongApplication.class, args);
+	}
+
+}
---
+++ soragodong/src/test/java/com/scit/soragodong/SoragodongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scit.soragodong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoragodongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
